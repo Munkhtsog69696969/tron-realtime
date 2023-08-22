f60024315a6b1d5f915f2b96ceb319c3dbe17f9b
heisenberg <sda@example.com>
2023-08-22 10:36:54 +0800

    solved connection coordinate problem

--- components/Game.jsx
+++ components/Game.jsx
@@ -5,8 +5,8 @@ import { ably } from '@/config/ablyConfig'
 import useInterval from 'use-interval'
 
 export default function Game() {
-  const data=sessionStorage.getItem("player");
-  const lobby=ably.channels.get(JSON.parse(data).lobby);
+  const playerData=JSON.parse(sessionStorage.getItem("player"));
+  const lobby=ably.channels.get(playerData.lobby);
   //map
   const [mapSize,setMapSize]=useState(0);
   const mapRef = useRef(null);
@@ -26,6 +26,10 @@ export default function Game() {
   //opponent
   const [opponentJoined,setOpponentJoined]=useState(false);
   const [opponent,setOpponent]=useState();
+  //my head and body
+  const [myHead,setMyHead]=useState();
+  const [myBody,setMyBody]=useState();
+  const [myDirection,setMyDirection]=useState();
   //drawing players
   const [drawPlayer1Head,setDrawPlayer1Head]=useState();
   const [drawPlayer1Body,setDrawPlayer1Body]=useState([]);
@@ -34,46 +38,58 @@ export default function Game() {
 
   function transferData(){
     lobby.publish(
-      JSON.parse(data).name ,
+      playerData.name ,
       {
-        direction1:playerDirection,
-        body:playerBodyCoords,
-        head:playerCoordinate,
-        number:JSON.parse(data).number,
-
-        direction2:player2Direction,
-        body2:player2BodyCoords,
-        head2:player2Coordinate,
-        number2:JSON.parse(data).number
+        number:playerData.number ,
+        head:myHead,
+        body:myBody,
+        direction:myDirection
       }
     )
   }
 
   useEffect(()=>{
-    // transferData();
+    transferData();
   },[])
 
+  //lisents
   lobby.subscribe((message)=>{
-    if(JSON.parse(data).name && message.name!==JSON.parse(data).name){
-      //oponnent joined
+    if(message.name!=playerData.name){
       setOpponentJoined(true);
-      //am i red or blue ? blue<red
-      if(JSON.parse(data).number<message.data.number){
-        //my opponent has bigger number so im blue
+
+      if(message.data.number>=playerData.number){
+        //opponent is red
         setMyColor("blue");
-        setDrawPlayer1Head(message.data.head);
-        setDrawPlayer2Head(message.data.head2);
+        setMyHead(playerCoordinate);
+        setMyBody(playerBodyCoords);
+        setMyDirection(playerDirection);
 
-
+        setDrawPlayer1Head(playerCoordinate);
+        setDrawPlayer1Body(playerBodyCoords);
+        setDrawPlayer2Head(message.data.head);
+        setDrawPlayer2Body(message.data.body)
       }else{
-        //my opoonent has lesser number so im red
-        setMyColor("red")
+        //opponent is blue
+        setMyColor("red");
+        setMyHead(player2Coordinate);
+        setMyBody(player2BodyCoords);
+        setMyDirection(player2Direction);
+
+        setDrawPlayer1Head(message.data.head);
+        setDrawPlayer1Body(message.data.body);
+        setDrawPlayer2Head(player2Coordinate);
+        setDrawPlayer2Body(player2BodyCoords)
       }
-    }else{
-
     }
+    console.log(message)
 
+    // if(opponentJoined){
+
+    // }
   });
+
+  console.log("1",playerDirection)
+  console.log("2",player2Direction)
 
   useEffect(()=>{
     if(mapRef){
@@ -141,10 +157,9 @@ export default function Game() {
     })
 
   },[]);
-  // console.log(myColor)
 
   function PlayerMovement(){
-    if(playerDirection=="right"){
+    if(myColor=="blue" && myDirection=="right"){
       if(playerCoordinate.left+index<mapSize){
         setPlayerCoordinate({left:playerCoordinate.left+index , top:playerCoordinate.top})
       }else{
@@ -152,7 +167,7 @@ export default function Game() {
       }
     }
 
-    if(playerDirection=="left"){
+    if(myColor=="blue" && myDirection=="left"){
       if(playerCoordinate.left!=0){
         setPlayerCoordinate({left:playerCoordinate.left-index , top:playerCoordinate.top})
       }else{
@@ -160,7 +175,7 @@ export default function Game() {
       }
     }
 
-    if(playerDirection=="down"){
+    if(myColor=="blue" && myDirection=="down"){
       if(playerCoordinate.top+index<mapSize){
         setPlayerCoordinate({left:playerCoordinate.left , top:playerCoordinate.top+index})
       }else{
@@ -168,7 +183,7 @@ export default function Game() {
       }
     }
 
-    if(playerDirection=="up"){
+    if(myColor=="blue" && myDirection=="up"){
       if(playerCoordinate.top!=0){
         setPlayerCoordinate({left:playerCoordinate.left , top:playerCoordinate.top-index})
       }else{
@@ -178,7 +193,7 @@ export default function Game() {
   }
 
   function Player2Movement(){
-    if(player2Direction=="right"){
+    if(myColor=="red" && myDirection=="right"){
       if(player2Coordinate.left+index<mapSize){
         setPlayer2Coordinate({left:player2Coordinate.left+index , top:player2Coordinate.top})
       }else{
@@ -186,7 +201,7 @@ export default function Game() {
       }
     }
 
-    if(player2Direction=="left"){
+    if(myColor=="red" && myDirection=="left"){
       if(player2Coordinate.left!=0){
         setPlayer2Coordinate({left:player2Coordinate.left-index , top:player2Coordinate.top})
       }else{
@@ -194,7 +209,7 @@ export default function Game() {
       }
     }
 
-    if(player2Direction=="down"){
+    if(myColor=="red" && myDirection=="down"){
       if(player2Coordinate.top+index<mapSize){
         setPlayer2Coordinate({left:player2Coordinate.left , top:player2Coordinate.top+index})
       }else{
@@ -202,7 +217,7 @@ export default function Game() {
       }
     }
 
-    if(player2Direction=="up"){
+    if(myColor=="red" && myDirection=="up"){
       if(player2Coordinate.top!=0){
         setPlayer2Coordinate({left:player2Coordinate.left , top:player2Coordinate.top-index})
       }else{
@@ -217,16 +232,14 @@ export default function Game() {
       Player2Movement();
       setPlayerBodyCoords([...playerBodyCoords , {left:playerCoordinate.left , top:playerCoordinate.top}]);
       setPlayer2BodyCoords([...player2BodyCoords , {left:player2Coordinate.left , top:player2Coordinate.top}]);
-      transferData()
     }
-  },1000);
-
-  // if(isDead){
-  //   alert("Dead")
-  // }
+    transferData()
+  },400);
 
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-red-300'>
+      <div className='fixed top-8'>Your color is {myColor&&myColor}</div>
+      <div className='fixed top-0'>{opponentJoined ? "opponent joined" : "opponent not joined"}</div>
       <div ref={mapRef} className='w-600 h-600 bg-orange-300 relative'>
         {/* head */}
         <div
@@ -235,13 +248,13 @@ export default function Game() {
             height:index&&index,
             backgroundColor:"blue",
             position:"absolute",
-            left:playerCoordinate.left,
-            top:playerCoordinate.top,
+            left:drawPlayer1Head&&drawPlayer1Head.left,
+            top:drawPlayer1Head&&drawPlayer1Head.top,
             transition:"200ms"
           }}
         ></div>
         {
-          playerBodyCoords.map((body,i)=>{
+          drawPlayer1Body&&drawPlayer1Body.map((body,i)=>{
             return(
               <div
                 style={{
@@ -266,13 +279,13 @@ export default function Game() {
             height:index&&index,
             backgroundColor:"red",
             position:"absolute",
-            left:player2Coordinate.left,
-            top:player2Coordinate.top,
+            left:drawPlayer2Head&&drawPlayer2Head.left,
+            top:drawPlayer2Head&&drawPlayer2Head.top,
             transition:"200ms"
           }}
         ></div>
         {
-          player2BodyCoords.map((body,i)=>{
+          drawPlayer2Body&&drawPlayer2Body.map((body,i)=>{
             return(
               <div
                 style={{
@@ -290,6 +303,8 @@ export default function Game() {
             )
           })
         }
+
+
       </div>
     </div>
   )
